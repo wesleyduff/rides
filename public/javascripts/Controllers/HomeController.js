6c355b792965655f8334b1fd71a2ee2d19aa7be3
slysop <slysop@gmail.com>
2013-12-04 14:29:56 -0600

    cleanup best practice angular

--- public/javascripts/Controllers/HomeController.js
+++ public/javascripts/Controllers/HomeController.js
@@ -23,17 +23,40 @@ app //Our module. Assigned as a global variable in scripts.js
 //           email : response.email
 //       };
 //    });
+    /* -------------------
+     Binded scope objects
+     ng-bind
+     */
+    $scope.userName = null;
+    /* -------------------
+     HOOOKS
+     ng-show, ng-hide
+     */
+        $scope.canShowLogin = true;
+        $scope.canShowLoginSuccessView = false;
 
-       /* -------------------
-        On click event to register a new user.
-        Created in the register-form.jade template
-       */
-      $scope.doRegister = function(){
-          /* Check to see if form is prestine */
-          if ($scope.registerForm.$dirty && $scope.registerForm.$valid) {
-              console.info("clean : " +  $scope.registerForm);
-          } else {
-              console.info($scope.registerForm);
-          }
-      };
+    /* -------------------
+    On click event to register a new user.
+    Created in the register-form.jade template
+    */
+    $scope.doRegister = function(){
+      /* Check to see if form is prestine */
+      if ($scope.registerForm.$dirty && $scope.registerForm.$valid) {
+          userFactoryResponse.saveUser({
+            "name" : this.name,
+            "email": this.email,
+            "password" : this.password,
+            "modifiedOn" : Date.now(),
+            "lastLogin" : Date.now(),
+            "cat" : this.cat
+          }).then(function(response){
+            $scope.canShowLogin = false;
+            $('#register-form button.close').click();
+            $scope.userName = response.name;
+            $scope.canShowLoginSuccessView = true;
+          });
+      } else {
+          console.info($scope.registerForm);
+      }
+    };
 }]);
